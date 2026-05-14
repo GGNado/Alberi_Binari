4896d1c297e43f8663d7a1e665c12b6b770b77ca
ggnado <luigimassa2005@gmail.com>
2026-05-14 16:38:16 +0200

    Refactor e aggiunti alberi n-ari

--- src/entity/BS.java
+++ src/entity/BS.java
@@ -1,40 +1,8 @@
 package entity;
 
-import java.util.Scanner;
-
 public class BS extends Nodo<BS> {
 
     public BS(int valore) {
         super(valore);
     }
-
-    public void populate(BS nodo, Scanner scanner){
-        if (nodo == null){
-            System.out.println("caso base");
-            return;
-        }
-
-        System.out.println("Inserisci il valore del nodo: ");
-        int valore = Integer.parseInt(scanner.nextLine());
-        nodo.setValore(valore);
-
-        System.out.println("Vuoi inserire alla Sinistra di " + nodo.getValore() + "? (s/n)");
-        String risposta = scanner.nextLine();
-        if (risposta.equals("s")){
-            nodo.setSinistro(new BS(0));
-            populate(nodo.getSinistro(), scanner);
-        }
-
-        System.out.println("Vuoi inserire alla Destra di " + nodo.getValore() + "? (s/n)");
-        risposta = scanner.nextLine();
-        if (risposta.equals("s")){
-            nodo.setDestra(new BS(0));
-            populate(nodo.getDestra(), scanner);
-        }
-
-        System.out.println("Finito di inserire sia sinistra che destra di " + nodo.getValore());
-
-    }
 }
-
-
---
+++ src/entity/NT.java
@@ -0,0 +1,18 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Getter
+@Setter
+public class NT {
+    private int valore;
+    private List<NT> figli;
+}
